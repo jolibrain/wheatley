7b1c0490a6d7ef8b9483d965cb982aeb983c68b5
Michel Thomazo <thomazo.michel@gmail.com>
2021-08-11 12:14:39 +0200

    change shape of output for agent

--- models/actor_critic.py
+++ models/actor_critic.py
@@ -55,6 +55,7 @@ class ActorCritic(nn.Module):
             dim=1,
         )
         batch_size = graph_embedding.shape[0]
+        n_nodes = nodes_embedding.shape[1]
 
         value = self.critic(graph_embedding)
 
@@ -64,23 +65,26 @@ class ActorCritic(nn.Module):
         probabilities = self.actor(possible_s_a_pairs)
         pi = F.softmax(probabilities, dim=1)
 
-        # Just to test, not correct
-        pi = torch.cat(
-            (pi, torch.zeros(batch_size, MAX_N_NODES * MAX_N_NODES - pi.shape[1], 1, device=DEVICE)),
-            dim=1,
-        )
-        pi = pi.reshape(batch_size, MAX_N_NODES * MAX_N_NODES)
+        # The final pi must include all actions (even those that are not applicable
+        # because of the size of the graph). So we convert the flattened pi to a square
+        # matrix of size (n_nodes, n_nodes). We then complete the matrix to get a square
+        # matrix of size (MAX_N_NODES, MAX_N_NODES) with 0, and we reflatten it.
+        pi = pi.reshape(batch_size, n_nodes * n_nodes)  # Remove the dim 2 of size 1
+        pi = pi.reshape(batch_size, n_nodes, n_nodes)
+        filled_pi = torch.zeros(batch_size, MAX_N_NODES, MAX_N_NODES, device=DEVICE)
+        filled_pi[:, 0:n_nodes, 0:n_nodes] = pi
+        filled_pi = filled_pi.reshape(batch_size, MAX_N_NODES * MAX_N_NODES)
 
-        return pi, value
+        return filled_pi, value
 
     def compute_possible_s_a_pairs(self, graph_embedding, nodes_embedding):
         # We create 3 tensors representing state, node 1 and node 2
         # and then stack them together to get all state action pairs
         n_nodes = nodes_embedding.shape[1]
-        
+
         states = graph_embedding.repeat(1, n_nodes * n_nodes, 1)
         nodes1 = nodes_embedding.repeat(1, n_nodes, 1)
         nodes2 = nodes_embedding.repeat_interleave(n_nodes, dim=1)
-        
+
         s_a_pairs = torch.cat([states, nodes1, nodes2], dim=2)
         return s_a_pairs
