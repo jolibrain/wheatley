2213bcc8eb37f696d8c1fe9be385d228591b29d6
Michel Thomazo <thomazo.michel@gmail.com>
2021-08-17 14:47:34 +0200

    remove usless args in policy.py

--- models/policy.py
+++ models/policy.py
@@ -5,12 +5,8 @@ from models.actor_critic import ActorCritic
 
 
 class Policy(ActorCriticPolicy):
-    def __init__(
-        self, observation_space, action_space, lr_schedule, *args, **kwargs
-    ):
-        super(Policy, self).__init__(
-            observation_space, action_space, lr_schedule, *args, **kwargs
-        )
+    def __init__(self, *args, **kwargs):
+        super(Policy, self).__init__(*args, **kwargs)
 
     def _build_mlp_extractor(self):
         self.mlp_extractor = ActorCritic()
