b113792ed998a8ecbbbb73f75ff1f99751f968ed
Michel Thomazo <thomazo.michel@gmail.com>
2021-08-17 14:31:15 +0200

    modify forward of policy to exclude the final layers that was applied

--- models/policy.py
+++ models/policy.py
@@ -1,13 +1,32 @@
+from stable_baselines3.common.distributions import CategoricalDistribution
 from stable_baselines3.common.policies import ActorCriticPolicy
 
 from models.actor_critic import ActorCritic
 
 
 class Policy(ActorCriticPolicy):
-    def __init__(self, observation_space, action_space, lr_schedule, *args, **kwargs):
+    def __init__(
+        self, observation_space, action_space, lr_schedule, *args, **kwargs
+    ):
         super(Policy, self).__init__(
             observation_space, action_space, lr_schedule, *args, **kwargs
         )
 
     def _build_mlp_extractor(self):
         self.mlp_extractor = ActorCritic()
+
+    def forward(self, obs, deterministic=False):
+        """Forward pass in all the networks (actor and critic)
+        Note : this is a reimplementation of the forward function of
+        stable_baselines3.common.polcies.ActorCriticPolicy, in order to compute actor
+        and critic net in a certain way.
+        """
+        latent_pi, latent_vf, latent_sde = self._get_latent(obs)
+        # Evaluate the values for the given observation
+        values = latent_vf  # Modification here
+        distribution = self.action_dist.proba_distribution(
+            action_logits=latent_pi
+        )
+        actions = distribution.get_actions(deterministic=deterministic)
+        log_prob = distribution.log_prob(actions)
+        return actions, values, log_prob
