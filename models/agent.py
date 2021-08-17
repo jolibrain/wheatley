aed6497ffa822184e9435956b9c3249f62486fe5
Michel Thomazo <thomazo.michel@gmail.com>
2021-08-17 16:58:18 +0200

    continue implementation

--- models/agent.py
+++ models/agent.py
@@ -7,10 +7,27 @@ from config import DEVICE
 
 
 class Agent:
-    def __init__(self, env):
-        self.model = PPO(
-            Policy,
-            env,
-            policy_kwargs={"features_extractor_class": FeaturesExtractor},
-            device=DEVICE,
-        )
+    def __init__(self, env, model=None):
+        if model is not None:
+            self.model = model
+            self.model.set_env(env)
+        else:
+            self.model = PPO(
+                Policy,
+                env,
+                policy_kwargs={"features_extractor_class": FeaturesExtractor},
+                device=DEVICE,
+            )
+
+    @classmethod
+    def load(cls, path, env):
+        return cls(env, PPO.load(path, env, DEVICE))
+
+    def save(self, path):
+        self.model.save(path)
+
+    def train(self, problem_description, n_steps):
+        pass
+
+    def predict(self, problem_description):
+        pass
