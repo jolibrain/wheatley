b7878c8fdfa6c1831582615ca971f1cd5aa98b64
Michel Thomazo <thomazo.michel@gmail.com>
2021-08-17 17:07:32 +0200

    continue implementation of Agent

--- models/agent.py
+++ models/agent.py
@@ -1,5 +1,6 @@
 from stable_baselines3.ppo import PPO
 
+from env.env import Env
 from models.policy import Policy
 from models.features_extractor import FeaturesExtractor
 
@@ -19,15 +20,25 @@ class Agent:
                 device=DEVICE,
             )
 
+    def save(self, path):
+        self.model.save(path)
+
     @classmethod
     def load(cls, path, env):
         return cls(env, PPO.load(path, env, DEVICE))
 
-    def save(self, path):
-        self.model.save(path)
-
-    def train(self, problem_description, n_steps):
-        pass
+    def train(self, problem_description, total_timesteps):
+        env = Env(problem_description)
+        self.model.set_env(env)
+        self.model.learn(total_timesteps)
 
     def predict(self, problem_description):
-        pass
+        env = Env(problem_description)
+        self.model.set_env(env)
+        observation = env.reset()
+        done = False
+        while not done:
+            action, _ = self.model.predict(observation)
+            observation, reward, done, info = env.step(action)
+        solution = env.get_solution()
+        return solution
