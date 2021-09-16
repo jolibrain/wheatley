da72d06d11c6c60517a2e7f07153952aa789e080
Michel Thomazo <thomazo.michel@gmail.com>
2021-09-16 11:28:58 +0200

    reformat args.py

--- args.py
+++ args.py
@@ -8,101 +8,33 @@ parser = argparse.ArgumentParser(
 parser.add_argument("--n_j", type=int, default=5, help="Number of jobs")
 parser.add_argument("--n_m", type=int, default=5, help="Number of machines")
 parser.add_argument("--seed", type=int, default=42, help="Random seed")
-parser.add_argument(
-    "--path",
-    type=str,
-    default="saved_networks/default_net",
-    help="Path to saved model",
-)
-parser.add_argument(
-    "--add_machine_id",
-    default=False,
-    action="store_true",
-    help="Add the machine id in the node embedding",
-)
+parser.add_argument("--path", type=str, default="saved_networks/default_net", help="Path to saved model")
 
-# Training and testing argument
-parser.add_argument(
-    "--fixed_benchmark",
-    default=False,
-    action="store_true",
-    help="Wether to test model on fixed or random becnhmark",
-)
+parser.add_argument("--add_machine_id", default=False, action="store_true", help="Add the machine id in the node embedding")
+parser.add_argument("--fixed_benchmark", default=False, action="store_true", help="Test model on fixed or random benchmark")
 
 # Training arguments
-parser.add_argument(
-    "--total_timesteps",
-    type=int,
-    default=int(1e4),
-    help="Number of training timesteps (corresponding to env timesteps)",
-)
-parser.add_argument(
-    "--n_test_env",
-    type=int,
-    default=5,
-    help="Number of testing environments during traing",
-)
-parser.add_argument(
-    "--eval_freq",
-    type=int,
-    default=200,
-    help="Evaluation of model is made every eval_freq timesteps",
-)
-parser.add_argument(
-    "--n_epochs",
-    type=int,
-    default=1,
-    help="Number of epochs for updating the PPO parameters",
-)
-parser.add_argument(
-    "--n_steps_episode",
-    type=int,
-    default=256,
-    help="Number of steps per episode. This should be bigger than the average number of steps in 1 env run.",
-)
-parser.add_argument(
-    "--batch_size",
-    type=int,
-    default=64,
-    help="Batch size during training of PPO",
-)
+parser.add_argument("--total_timesteps", type=int, default=int(1e4), help="Number of training env timesteps")
+parser.add_argument("--n_epochs", type=int, default=1, help="Number of epochs for updating the PPO parameters")
+parser.add_argument("--n_steps_episode", type=int, default=256, help="Number of steps per episode.")
+parser.add_argument("--batch_size", type=int, default=64, help="Batch size during training of PPO")
 parser.add_argument("--gamma", type=float, default=1, help="Discount factor")
 parser.add_argument("--clip_range", type=float, default=0.2, help="Clipping parameter")
 parser.add_argument("--ent_coef", type=float, default=0.005, help="Entropy coefficient")
 parser.add_argument("--vf_coef", type=float, default=0.5, help="Value function coefficient")
 parser.add_argument("--lr", type=float, default=2e-5, help="Learning rate")
-parser.add_argument(
-    "--dont_divide_loss",
-    default=False,
-    action="store_true",
-    help="Remove the dividing loss hack",
-)
-parser.add_argument(
-    "--fixed_problem",
-    default=False,
-    action="store_true",
-    help="Fix affectations and durations",
-)
-parser.add_argument(
-    "--n_workers",
-    type=int,
-    default=1,
-    help="Number of CPU cores for simulating environment",
-)
-parser.add_argument(
-    "--multiprocessing",
-    default=False,
-    action="store_true",
-    help="Wether to use multiprocessing or not",
-)
+
+parser.add_argument("--n_test_env", type=int, default=5, help="Number of testing environments during traing")
+parser.add_argument("--eval_freq", type=int, default=200, help="Number of steps between each evaluation during training")
+
+parser.add_argument("--dont_divide_loss", default=False, action="store_true", help="Don't divide loss by a constant")
+parser.add_argument("--fixed_problem", default=False, action="store_true", help="Fix affectations and durations for train")
+
+parser.add_argument("--n_workers", type=int, default=1, help="Number of CPU cores for simulating environment")
+parser.add_argument("--multiprocessing", default=False, action="store_true", help="Wether to use multiprocessing or not")
 
 # Testing arguments
-parser.add_argument(
-    "--n_test_problems",
-    type=int,
-    default=100,
-    help="Number of problems for testing",
-)
+parser.add_argument("--n_test_problems", type=int, default=100, help="Number of problems for testing")
 
 # Debug options
 parser.add_argument(
