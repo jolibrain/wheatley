jolibrain/wheatley

commit 781fde6aa5b230c076f0baf1b051f658f535dff3

correct the for loops : relax condition and add a break to take the first occurence of available time slot

env/l2d_transition_model.py
@@ -57,11 +57,11 @@ class L2DTransitionModel(TransitionModel):
                     start_time, duration, _ = machine_occupancy[i]
                     next_start_time, next_duration, _ = machine_occupancy[i + 1]
                     if start_time + duration < next_start_time:
-                        if (
-                            start_time + duration <= job_availability_time
-                            and job_availability_time + job_duration < next_start_time
+                        if start_time + duration <= job_availability_time and job_duration <= next_start_time - (
+                            start_time + duration
                         ):
                             index = i
+                            break
                 if index == -1:
                     # The job can be inserted nowhere, so we add it at the end
                     self.state.set_precedency(machine_occupancy[-1][2], node_id)
@@ -86,8 +86,9 @@ class L2DTransitionModel(TransitionModel):
                     start_time, duration, _ = machine_occupancy[i]
                     next_start_time, next_duration, _ = machine_occupancy[i + 1]
                     if start_time + duration < next_start_time:
-                        if start_time + duration <= job_availability_time and job_availability_time < next_start_time:
+                        if job_availability_time < next_start_time:
                             index = i
+                            break
                 if index == -1:
                     # The job can be inserted nowhere, so we add it at the end
                     self.state.set_precedency(machine_occupancy[-1][2], node_id)

commit 27fca78e3882022969b35911b616b6e7a3e01287

correct bug introduced in commit 781fde6aa5b230c076f0baf1b051f658f535dff3

env/l2d_transition_model.py
@@ -57,9 +57,7 @@ class L2DTransitionModel(TransitionModel):
                     start_time, duration, _ = machine_occupancy[i]
                     next_start_time, next_duration, _ = machine_occupancy[i + 1]
                     if start_time + duration < next_start_time:
-                        if start_time + duration <= job_availability_time and job_duration <= next_start_time - (
-                            start_time + duration
-                        ):
+                        if job_duration <= next_start_time - max(start_time + duration, job_availability_time):
                             index = i
                             break
                 if index == -1: