small fixes wrt normalization and reward

env/reward_models/l2d_reward_model.py
@@ -15,7 +15,12 @@ class L2DRewardModel(RewardModel):
         features_t = obs.features
         features_tp = next_obs.features
 
-        H_st = torch.max(features_t[:, 4]).item()
-        H_stp = torch.max(features_tp[:, 4]).item()
+        H_st = torch.max(features_t[:, 1]).item()
+        if H_st < 0:
+            H_st = 0
+        H_stp = torch.max(features_tp[:, 1]).item()
+        if H_stp < 0:
+            H_stp = 0
         reward = H_st - H_stp
-        return reward
+
+        return -reward

env/state.py
@@ -153,7 +153,7 @@ class State:
         self.n_machines_per_job = np.array(
             [self.n_machines - (self.affectations[j] == -1).sum() for j in range(self.n_jobs)]
         )
-        self.max_duration = np.max(self.durations.flatten())
+        self.max_duration = np.max(self.original_durations.flatten())
         self.max_completion_time = torch.max(self.features[:, 1:5].flatten()).item()
 
         self.compute_pre_features()
@@ -288,12 +288,11 @@ class State:
 
     def normalize_features(self, normalize):
         if not normalize:
-            return self.features
+            return self.features.clone()
         else:
             features = self.features.clone()
-            features[:, 1:5] /= self.max_duration
+            features[:, 1:5] /= self.max_completion_time
             features[:, 1] = torch.where(features[:, 1] < 0, torch.Tensor([-1.0]), features[:, 1])
-            # if "duration" in self.features_offset:
             try:
                 dof = self.features_offset["duration"]
                 features[:, dof[0] : dof[1]] /= self.max_duration

models/features_extractor.py
@@ -1,11 +1,11 @@
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 import torch
 import torch.nn as nn
-from torch_geometric.nn.conv import GINConv, GATv2Conv, EGConv, PDNConv
+from torch_geometric.nn.conv import GINEConv, GATv2Conv, EGConv, PDNConv
 
 from models.mlp import MLP
 from utils.agent_observation import AgentObservation
-from torch_geometric.nn.norm import GraphNorm
+from torch_geometric.nn.norm import GraphNorm, BatchNorm
 import sys
 
 
@@ -22,6 +22,7 @@ class FeaturesExtractor(BaseFeaturesExtractor):
         max_n_nodes,
         max_n_machines,
         n_mlp_layers_features_extractor,
+        activation_features_extractor,
         n_layers_features_extractor,
         hidden_dim_features_extractor,
         n_attention_heads,
@@ -100,17 +101,21 @@ class FeaturesExtractor(BaseFeaturesExtractor):
                 self.normsbis.append(GraphNorm(hidden_dim_features_extractor))
 
             if self.gconv_type == "gin":
+                mlp = torch.nn.Sequential()
+                for _ in range(n_mlp_layers_features_extractor - 1):
+                    mlp.append(torch.nn.Linear(hidden_dim_features_extractor, hidden_dim_features_extractor))
+                    mlp.append(activation_features_extractor())
+                    if normalize:
+                        mlp.append(BatchNorm(hidden_dim_features_extractor))
+
+                mlp.append(torch.nn.Linear(hidden_dim_features_extractor, hidden_dim_features_extractor))
+                if normalize:
+                    mlp.append(BatchNorm(hidden_dim_features_extractor))
+
                 self.features_extractors.append(
-                    GINConv(
-                        MLP(
-                            n_layers=n_mlp_layers_features_extractor,
-                            input_dim=hidden_dim_features_extractor,
-                            hidden_dim=hidden_dim_features_extractor,
-                            output_dim=hidden_dim_features_extractor,
-                            batch_norm=False if self.freeze_graph else True,
-                            activation="relu",
-                            device=self.device,
-                        )
+                    GINEConv(
+                        mlp,
+                        edge_dim=hidden_dim_features_extractor,
                     )
                 )
 