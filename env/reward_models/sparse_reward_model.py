5907cb55c5af2e37421e113c72b3ceb322d6402b
Guillaume Infantes <guillaume.infantes@jolibrain.com>
2023-03-03 08:16:03 +0000

    fix(reward): fix sparse reward in case of phantom tasks (machine == -1)

--- env/reward_models/sparse_reward_model.py
+++ env/reward_models/sparse_reward_model.py
@@ -12,7 +12,10 @@ class SparseRewardModel(RewardModel):
         Reward is 0 for every time steps, except for the last one, where it is the opposite of the Makespan
         """
         features_tp = next_obs.features
-        is_done = (features_tp[:, 0] == 1).all().item()
+        # below everything seleced, ok only for jssp, not for -1 in task affectration
+        # is_done = (features_tp[:, 0] == 1).all().item()
+        # nothing selectable anymore
+        is_done = (features_tp[:, 5] == 0).all().item()
         if not is_done:
             return 0
         makespan = torch.max(features_tp[:, 1]).item()
