3cc847562fd613a05c5c2cd9afbcf0105cd1c6cf
Guillaume Infantes <guillaume.infantes@jolibrain.com>
2023-03-20 14:33:52 +0000

    chore: fix license on dadapt_adam

--- models/dadapt_adam.py
+++ models/dadapt_adam.py
@@ -1,34 +1,24 @@
-#
-# Wheatley
-# Copyright (c) 2023 Jolibrain
-# Authors:
-#    Guillaume Infantes <guillaume.infantes@jolibrain.com>
-#    Antoine Jacquet <antoine.jacquet@jolibrain.com>
-#    Michel Thomazo <thomazo.michel@gmail.com>
-#    Emmanuel Benazera <emmanuel.benazera@jolibrain.com>
-#
-#
-# This file is part of Wheatley.
-#
-# Wheatley is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Wheatley is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Wheatley. If not, see <https://www.gnu.org/licenses/>.
-#
+# MIT License
 
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-# 
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import math
 from typing import TYPE_CHECKING, Any, Callable, Optional
@@ -44,38 +34,47 @@ if TYPE_CHECKING:
 else:
     _params_t = Any
 
+
 class DAdaptAdam(torch.optim.Optimizer):
     r"""
     Implements Adam with D-Adaptation automatic step-sizes. Leave LR set to 1 unless you encounter instability.
     Arguments:
-        params (iterable): 
+        params (iterable):
             Iterable of parameters to optimize or dicts defining parameter groups.
-        lr (float): 
+        lr (float):
             Learning rate adjustment parameter. Increases or decreases the D-adapted learning rate.
         betas (Tuple[float, float], optional): coefficients used for computing
             running averages of gradient and its square (default: (0.9, 0.999))
-        momentum (float): 
+        momentum (float):
             Momentum value in  the range [0,1) (default: 0.9).
-        eps (float): 
+        eps (float):
             Term added to the denominator outside of the root operation to improve numerical stability. (default: 0).
-        weight_decay (float): 
+        weight_decay (float):
             Weight decay, i.e. a L2 penalty (default: 0).
-        log_every (int): 
+        log_every (int):
             Log using print every k steps, default 0 (no logging).
-        decouple (boolean): 
+        decouple (boolean):
             Use AdamW style decoupled weight decay
         d0 (float):
             Initial D estimate for D-adaptation (default 1e-6). Rarely needs changing.
         growth_rate (float):
-            prevent the D estimate from growing faster than this multiplicative rate. 
+            prevent the D estimate from growing faster than this multiplicative rate.
             Default is inf, for unrestricted. Values like 1.02 give a kind of learning
             rate warmup effect.
     """
-    def __init__(self, params, lr=1.0, 
-                 betas=(0.9, 0.999), eps=1e-8,
-                 weight_decay=0, log_every=0,
-                 decouple=False,
-                 d0=1e-6, growth_rate=float('inf')):
+
+    def __init__(
+        self,
+        params,
+        lr=1.0,
+        betas=(0.9, 0.999),
+        eps=1e-8,
+        weight_decay=0,
+        log_every=0,
+        decouple=False,
+        d0=1e-6,
+        growth_rate=float("inf"),
+    ):
         if not 0.0 <= lr:
             raise ValueError("Invalid learning rate: {}".format(lr))
         if not 0.0 <= eps:
@@ -88,16 +87,19 @@ class DAdaptAdam(torch.optim.Optimizer):
         if decouple:
             print(f"Using decoupled weight decay")
 
-        
-        defaults = dict(lr=lr, betas=betas, eps=eps,
-                        weight_decay=weight_decay,
-                        d = d0, 
-                        k=0, 
-                        gsq_weighted=0.0,
-                        log_every=log_every,
-                        growth_rate=growth_rate,
-                        decouple=decouple)
-        
+        defaults = dict(
+            lr=lr,
+            betas=betas,
+            eps=eps,
+            weight_decay=weight_decay,
+            d=d0,
+            k=0,
+            gsq_weighted=0.0,
+            log_every=log_every,
+            growth_rate=growth_rate,
+            decouple=decouple,
+        )
+
         super().__init__(params, defaults)
 
     @property
@@ -119,7 +121,6 @@ class DAdaptAdam(torch.optim.Optimizer):
         if closure is not None:
             loss = closure()
 
-
         g_sq = 0.0
         sksq_weighted = 0.0
         sk_l1 = 0.0
@@ -127,27 +128,27 @@ class DAdaptAdam(torch.optim.Optimizer):
         ngroups = len(self.param_groups)
 
         group = self.param_groups[0]
-        gsq_weighted = group['gsq_weighted']
-        d = group['d']
-        lr = group['lr']
-        dlr = d*lr
-        
-        growth_rate = group['growth_rate']
-        decouple = group['decouple']
-        log_every = group['log_every']
+        gsq_weighted = group["gsq_weighted"]
+        d = group["d"]
+        lr = group["lr"]
+        dlr = d * lr
 
-        beta1, beta2 = group['betas']
+        growth_rate = group["growth_rate"]
+        decouple = group["decouple"]
+        log_every = group["log_every"]
+
+        beta1, beta2 = group["betas"]
 
         for group in self.param_groups:
-            decay = group['weight_decay']
-            k = group['k']
-            eps = group['eps']
+            decay = group["weight_decay"]
+            k = group["k"]
+            eps = group["eps"]
 
-            for p in group['params']:
+            for p in group["params"]:
                 if p.grad is None:
                     continue
                 grad = p.grad.data
-                
+
                 # Apply weight decay (coupled variant)
                 if decay != 0 and not decouple:
                     grad.add_(p.data, alpha=decay)
@@ -155,60 +156,61 @@ class DAdaptAdam(torch.optim.Optimizer):
                 state = self.state[p]
 
                 # State initialization
-                if 'step' not in state:
-                    state['step'] = 0
-                    state['s'] = torch.zeros_like(p.data).detach()
+                if "step" not in state:
+                    state["step"] = 0
+                    state["s"] = torch.zeros_like(p.data).detach()
                     # Exponential moving average of gradient values
-                    state['exp_avg'] = torch.zeros_like(p.data).detach()
+                    state["exp_avg"] = torch.zeros_like(p.data).detach()
                     # Exponential moving average of squared gradient values
-                    state['exp_avg_sq'] = torch.zeros_like(p.data).detach()
+                    state["exp_avg_sq"] = torch.zeros_like(p.data).detach()
 
-                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
-                
+                exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
 
                 # Adam EMA updates
-                exp_avg.mul_(beta1).add_(grad, alpha=dlr*(1-beta1))
-                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1-beta2)
-                
+                exp_avg.mul_(beta1).add_(grad, alpha=dlr * (1 - beta1))
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
+
                 denom = exp_avg_sq.sqrt().add_(eps)
 
                 g_sq += (grad * grad).div_(denom).sum().item()
 
-                s = state['s']
-                s.mul_(beta2).add_(grad, alpha=dlr*(1-beta2))
+                s = state["s"]
+                s.mul_(beta2).add_(grad, alpha=dlr * (1 - beta2))
                 sksq_weighted += (s * s).div_(denom).sum().item()
                 sk_l1 += s.abs().sum().item()
 
             ######
 
-        gsq_weighted = beta2*gsq_weighted + g_sq*(dlr**2)*(1-beta2)
+        gsq_weighted = beta2 * gsq_weighted + g_sq * (dlr**2) * (1 - beta2)
         d_hat = d
 
         if lr > 0.0:
-            d_hat = (sksq_weighted/(1-beta2) - gsq_weighted)/sk_l1
-            d = max(d, min(d_hat, d*growth_rate))
+            d_hat = (sksq_weighted / (1 - beta2) - gsq_weighted) / sk_l1
+            d = max(d, min(d_hat, d * growth_rate))
 
         if log_every > 0 and k % log_every == 0:
-            print(f"ng: {ngroups} lr: {lr} dlr: {dlr} d_hat: {d_hat}, d: {d}. sksq_weighted={sksq_weighted:1.1e} sk_l1={sk_l1:1.1e} gsq_weighted={gsq_weighted:1.1e}")
+            print(
+                f"ng: {ngroups} lr: {lr} dlr: {dlr} d_hat: {d_hat}, d: {d}. sksq_weighted={sksq_weighted:1.1e} sk_l1={sk_l1:1.1e} gsq_weighted={gsq_weighted:1.1e}"
+            )
 
         for group in self.param_groups:
-            group['gsq_weighted'] = gsq_weighted
-            group['d'] = d
+            group["gsq_weighted"] = gsq_weighted
+            group["d"] = d
 
-            decay = group['weight_decay']
-            k = group['k']
-            eps = group['eps']
+            decay = group["weight_decay"]
+            k = group["k"]
+            eps = group["eps"]
 
-            for p in group['params']:
+            for p in group["params"]:
                 if p.grad is None:
                     continue
                 grad = p.grad.data
 
                 state = self.state[p]
 
-                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
 
-                state['step'] += 1
+                state["step"] += 1
 
                 denom = exp_avg_sq.sqrt().add_(eps)
 
@@ -216,10 +218,9 @@ class DAdaptAdam(torch.optim.Optimizer):
                 if decay != 0 and decouple:
                     p.data.add_(p.data, alpha=-decay * dlr)
 
-
                 ### Take step
                 p.data.addcdiv_(exp_avg, denom, value=-1)
 
-            group['k'] = k + 1
+            group["k"] = k + 1
 
         return loss
