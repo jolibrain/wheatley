e8acd469b8f714ef3d09e7eb450c63a9a40a0411
Guillaume Infantes <guillaume.infantes@jolibrain.com>
2023-03-22 08:56:10 +0000

    refactor: use generic rebatch_obs

--- alg/ppo.py
+++ alg/ppo.py
@@ -33,7 +33,7 @@ import torch.optim as optim
 from .logger import Logger, configure_logger
 from env.env import Env
 from collections import deque
-from utils.utils import obs_as_tensor, safe_mean
+from utils.utils import obs_as_tensor, safe_mean, rebatch_obs
 import tqdm
 
 
@@ -78,70 +78,6 @@ class PPO:
         # in case of resume
         self._num_timesteps_at_start = 0
         self.ep_info_buffer = deque(maxlen=100)
-
-    def rebatch_obs(self, obs):
-        rebatched_obs = {}
-        max_nnodes = 0
-        max_nedges = 0
-        max_nconflicts_edges = 0
-        for _obs in obs:
-            mnn = _obs["n_nodes"].max().item()
-            if mnn > max_nnodes:
-                max_nnodes = mnn
-            mne = _obs["n_edges"].max().item()
-            if mne > max_nedges:
-                max_nedges = mne
-            if "n_conflict_edges" in _obs:
-                mce = _obs["n_conflict_edges"].max().item()
-                if mce > max_nconflicts_edges:
-                    max_nconflicts_edges = mce
-        max_nnodes = int(max_nnodes)
-        max_nedges = int(max_nedges)
-        max_nconflicts_edges = int(max_nconflicts_edges)
-
-        for key in obs[0]:
-            if key == "features":
-                s = (max_nnodes, obs[0][key].shape[-1])
-                rebatched_obs[key] = torch.stack(
-                    [
-                        torch.nn.functional.pad(
-                            obs[j][key],
-                            (0, 0, 0, max_nnodes - obs[j][key].shape[-2]),
-                        )
-                        for j in range(self.num_steps)
-                    ]
-                ).reshape(torch.Size((-1,)) + s)
-            elif key == "edge_index":
-                s = (obs[0][key].shape[-2], max_nedges)
-                rebatched_obs[key] = torch.stack(
-                    [
-                        torch.nn.functional.pad(
-                            obs[j][key],
-                            (0, max_nedges - obs[j][key].shape[-1]),
-                        )
-                        for j in range(self.num_steps)
-                    ]
-                ).reshape(torch.Size((-1,)) + s)
-            elif key in ["conflicts_edges", "conflicts_edges_machineid"]:
-                s = (obs[0][key].shape[-2], max_nconflicts_edges)
-                rebatched_obs[key] = torch.stack(
-                    [
-                        torch.nn.functional.pad(
-                            obs[j][key],
-                            (
-                                0,
-                                max_nconflicts_edges - obs[j][key].shape[-1],
-                            ),
-                        )
-                        for j in range(self.num_steps)
-                    ]
-                ).reshape(torch.Size((-1,)) + s)
-            else:
-                s = obs[0][key].shape
-                rebatched_obs[key] = torch.stack(
-                    [obs[j][key] for j in range(self.num_steps)]
-                ).reshape(torch.Size((-1,)) + s[1:])
-        return rebatched_obs
 
     def get_obs(self, b_obs, mb_ind):
         minibatched_obs = {}
@@ -220,7 +156,7 @@ class PPO:
             returns = advantages + values
 
         # flatten the batch
-        b_obs = self.rebatch_obs(obs)
+        b_obs = rebatch_obs(obs)
         b_logprobs = logprobs.reshape(-1)
         b_actions = actions.reshape((-1,) + envs.single_action_space.shape)
         b_advantages = advantages.reshape(-1)
