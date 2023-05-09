3a0a4f6e7fb494782d8e1b4326710ccfda0c4e0d
Pierre Pereira <pierre.pereira@jolibrain.com>
2023-05-09 18:05:14 +0200

    feat(benchmark): benchmarking dgl HPs

--- benchmark.py
+++ benchmark.py
@@ -1,10 +1,27 @@
+import json
+import random
 from copy import deepcopy
+from pathlib import Path
+from typing import Any, Dict
 
 from tqdm import tqdm
 
 from args import get_exp_name, get_path
 from instances.generate_taillard import generate_taillard, taillard_to_str
 from train import main
+
+
+def sample_hyperparams(hyperparams: Dict[str, list]) -> Dict[str, Any]:
+    sampled = dict()
+    for key, values in hyperparams.items():
+        sampled[key] = random.choice(values)
+    return sampled
+
+
+def log_hyperparams(hyperparams: Dict[str, Any], logfile: Path):
+    # Save the hyperparams in a json file.
+    with open(logfile, "w") as f:
+        json.dump(hyperparams, f, indent=4)
 
 
 def benchmark_seed(args, n_tries: int):
@@ -80,8 +97,50 @@ def benchmark_small_exp_to_big_exp_generalisation(args, n_tries: int):
         main(args, exp_name, path)
 
 
+def benchmark_dgl_hyperparams(args, n_tries: int):
+    params_space = {
+        "graph_pooling": ["max", "average", "learn"],
+        "layer_pooling": ["last", "all"],
+        "mlp_act_graph": ["relu", "tanh", "gelu", "selu"],
+        "n_mlp_layers_features_extractor": [1, 3, 5],
+        "n_layers_features_extractor": [1, 3, 6, 10],
+        "hidden_dim_features_extractor": [16, 32, 64],
+        "residual_gnn": [True, False],
+        "normalize_gnn": [True, False],
+        "fe_type": ["dgl"],
+        "n_mlp_layers_actor": [1],
+        "n_mlp_layers_critic": [1],
+        "hidden_dim_actor": [32],
+        "hidden_dim_critic": [32],
+        "graph_has_relu": [True],
+    }
+
+    for seed in tqdm(range(n_tries), desc="BENCHMARKING DGL HYPERPARAMS"):
+        args = deepcopy(args)
+
+        setattr(args, "seed", seed)
+
+        # Sample hyperparams.
+        sampled = sample_hyperparams(params_space)
+        for key, value in sampled.items():
+            setattr(args, key, value)
+
+        # Set specific args.
+        setattr(args, "exp_name_appendix", f"dgl-hyperparams_{seed}")
+        exp_name = get_exp_name(args)
+        path = get_path(args.path, exp_name)
+
+        # Save to log files.
+        setattr(args, "log_file", Path(path) / "visdom.jsonl")
+        log_hyperparams(sampled, Path(path) / "hyperparams.json")
+        setattr(args, "disable_visdom", True)
+
+        main(args, exp_name, path)
+
+
 if __name__ == "__main__":
     from args import args
 
     # benchmark_seed(args, 10)
-    benchmark_single_experiment_generalisation(args, 10)
+    # benchmark_single_experiment_generalisation(args, 10)
+    benchmark_dgl_hyperparams(args, 100)
