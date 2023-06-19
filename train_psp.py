3415333cc13624d00635193de431e32f38cdb8e0
Guillaume Infantes <guillaume.infantes@jolibrain.com>
2023-06-19 07:18:13 +0000

    useless factored options

--- train_psp.py
+++ train_psp.py
@@ -153,6 +153,8 @@ def main():
             do_not_observe_updated_bounds=args.do_not_observe_updated_bounds,
             # factored_rp=(args.fe_type == "tokengt" or args.factored_rp),
             factored_rp=args.factored_rp,
+            factored_rc=args.factored_rc,
+            forget_past=args.forget_past,
         )
         env_specification.print_self()
         if args.batch_size == 1 and not args.dont_normalize_advantage:
